3cfdad2ad2e6c2cc974fe9cad5f8bd58e1919f59
Petr DG <devil-ghost@yandex.ru>
2023-09-22 20:56:02 +0300

    Добавление файлов

---
+++ Index.js
@@ -0,0 +1,22 @@
+while (true) {
+
+        let anyNumber = Math.round(Math.random() * 1000);
+        console.log("Мы загадали ",anyNumber);
+
+        let clientNumber = prompt("Отгадайте число от 0 до 1000");
+
+       console.log("Клиент ответил ",clientNumber);
+        if (isNaN(clientNumber)){
+                alert("Вы ввели не число")
+        }
+        else if (clientNumber === null) {
+               break;
+        }
+           
+        else if (clientNumber == anyNumber) {
+            alert("Ура вы угадали!!!!!")
+        } 
+        else {
+            alert("Вы не угадали(((")
+        }
+    }
